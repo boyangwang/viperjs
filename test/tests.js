22b2e49dd80bd9539541db6b41af516a236b4fec
Boyang <wangboyang1991@gmail.com>
2016-09-14 00:38:22 +0800

    first integration test case, serve static index.html

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,28 @@
 'use strict';
-const assert = require('assert');
+const expect = require('chai').expect;
+const app = require('../app.js');
+const req = require('request-promise');
+const fs = require('fs');
+const path = require('path');
+
 describe('Sanity', () => {
     it('should be sane', () => {
-        assert.equal(true, true);
+        expect(true).equal(true);
+    });
+});
+describe('Viper.js integration', () => {
+    before((done) => {
+        app.server.on('listening', () => done());
+        if (app.server.listening) done();
+    });
+    it('should server static files', (done) => {
+        req({uri: 'http://localhost:7030' + '/index.html'})
+            .then((res) => new Promise((resolve, reject) => {
+                fs.readFile(path.join(__dirname, '../public', 'index.html'), 'utf8', (err, data) => {
+                    resolve({file: data, res: res});
+                });
+            }))
+            .then((results) => { expect(results.file).equal(results.res); })
+            .then(done).catch((reason) => done(reason));
     });
 });
