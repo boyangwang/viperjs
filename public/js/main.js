01aeb2c51637ed53a5e0de9313d6b7de29415180
Boyang <wangboyang1991@gmail.com>
2016-09-14 03:45:47 +0800

    require fonts.css, change id convention to use -

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
+require('../css/fonts.css');
 const React = require('react');
 const ReactDOM = require('react-dom');
 const ViperjsApp = require('./ViperjsApp.react.js');
 
-ReactDOM.render(React.createElement(ViperjsApp, null), document.getElementById('viperjsApp'));
+ReactDOM.render(React.createElement(ViperjsApp, null), document.getElementById('viperjs-app'));
