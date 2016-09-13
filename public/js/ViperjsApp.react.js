51786926c0c67b95249c6748ab29199cd87b026c
Boyang <wangboyang1991@gmail.com>
2016-09-14 01:43:26 +0800

    setup react minimal app

---
+++ public/js/ViperjsApp.react.js
@@ -0,0 +1,13 @@
+'use strict';
+const React = require('react');
+
+let TodoApp = module.exports = React.createClass({
+    displayName: 'ViperjsApp',
+    render: function() {
+        return React.createElement(
+            'div',
+            null,
+            'ViperjsApp'
+        );
+    }
+});
---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+const React = require('react');
+const ReactDOM = require('react-dom');
+const ViperjsApp = require('./ViperjsApp.react.js');
+
+ReactDOM.render(React.createElement(ViperjsApp, null), document.getElementById('viperjsApp'));
