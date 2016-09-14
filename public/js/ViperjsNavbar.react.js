acf935be2a9a8e563a39211e88fa9a6aec23c946
Boyang <wangboyang1991@gmail.com>
2016-09-14 22:14:50 +0800

    cleanup inline css

--- public/js/ViperjsNavbar.react.js
+++ public/js/ViperjsNavbar.react.js
@@ -4,68 +4,48 @@ const React = require('react');
 let ViperjsNavbar = module.exports = React.createClass({
     displayName: 'ViperjsNavbar',
     render: () => {
-        let navbarStyle = {
+        let commonMarginPaddingVerticalAlignBoxSizingStyle = {
+            border: '0',
+            boxSizing: 'border-box',
+            margin: '0',
+            padding: '0',
+            verticalAlign: 'baseline',
+        };
+        let navbarStyle = Object.assign({}, commonMarginPaddingVerticalAlignBoxSizingStyle, {
             background: 'rgba(55,61,73,0.975)',
             color: '#fff',
-            boxShadow: 'none',
-            position: 'fixed',
+            fontSize: '100%',
+            height: '3.75em',
             left: '0',
+            position: 'fixed',
             top: '0',
             width: '100%',
-            height: '3.75em',
-            zIndex: '10000',
-            transition: 'top 0.25s ease-in-out',
-            fontSize: '0.9em',
-            display: 'block',
-            margin: '0',
-            padding: '0',
-            border: '0',
-            verticalAlign: 'baseline',
-            boxSizing: 'border-box',
-            fontSize: '100%'
-        };
-        let navbarLeftRightCommonStyle = {
-            margin: '0',
-            padding: '0',
-            border: '0',
-            verticalAlign: 'baseline',
-            boxSizing: 'border-box',
+            zIndex: '9999',
+        });
+        let commonNavbarLeftRightStyle = Object.assign({}, commonMarginPaddingVerticalAlignBoxSizingStyle, {
+            overflow: 'hidden',
             position: 'absolute',
             top: '0.5em',
             whiteSpace: 'nowrap',
-            overflow: 'hidden',
-        };
-        let h1Style = {
-            fontSize: '2em',
-            letterSpacing: '-0.5px',
-            fontWeight: '600',
-            float: 'left',
-            lineHeight: '1.375em',
-            margin: '0 1.25em 0 0.5em',
+        });
+        let h1Style = Object.assign({}, commonMarginPaddingVerticalAlignBoxSizingStyle, {
             display: 'inline-block',
-            padding: '0',
-            border: '0',
-            verticalAlign: 'baseline',
-            boxSizing: 'border-box',
-            fontFamily: '"proxima-nova","Helvetica Neue",Helvetica,Arial,sans-serif'
-        };
-        let linksStyle = {
-            float: 'left',
-            marginLeft: '0.5em'
-        };
-        let linkStyle = {
-            outline: '0',
-            transition: 'color 0.2s ease-in-out,border-color 0.2s ease-in-out,background-color 0.2s ease-in-out',
-            display: 'inline-block',
-            height: '2.75em',
-            lineHeight: '2.75em',
-            padding: '0 1.5em 0 1.5em',
-            borderRadius: '4px',
-            textDecoration: 'none',
-            fontWeight: '600',
-            whiteSpace: 'nowrap',
             fontFamily: '"proxima-nova","Helvetica Neue",Helvetica,Arial,sans-serif',
+            fontSize: '2em',
+            fontWeight: '600',
+            letterSpacing: '-0.5px',
+            lineHeight: '1.375em',
+            margin: '0 0.5em',
+        });
+        let linkStyle = {
             color: '#fff',
+            display: 'inline-block',
+            fontFamily: '"proxima-nova","Helvetica Neue",Helvetica,Arial,sans-serif',
+            fontWeight: '600',
+            lineHeight: '2.75em',
+            padding: '0 1.5em',
+            textDecoration: 'none',
+            whiteSpace: 'nowrap',
         };
         return React.createElement(
             "header", {
@@ -77,7 +57,7 @@ let ViperjsNavbar = module.exports = React.createClass({
                     className: "viperjs-navbar-left",
                     style: Object.assign({
                         left: '0.5em'
-                    }, navbarLeftRightCommonStyle)
+                    }, commonNavbarLeftRightStyle)
                 },
                 React.createElement(
                     "h1", {
@@ -92,12 +72,11 @@ let ViperjsNavbar = module.exports = React.createClass({
                     className: "viperjs-navbar-right",
                     style: Object.assign({
                         right: '0.5em'
-                    }, navbarLeftRightCommonStyle)
+                    }, commonNavbarLeftRightStyle)
                 },
                 React.createElement(
                     "div", {
                         className: "viperjs-navbar-links",
-                        style: linksStyle
                     },
                     React.createElement(
                         "a", {
