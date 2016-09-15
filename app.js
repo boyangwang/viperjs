bb85b823116cd725434e5e1ca744fea82890d493
Boyang <wangboyang1991@gmail.com>
2016-09-16 05:43:02 +0800

    handle 404 by plain text Not found

--- app.js
+++ app.js
@@ -2,6 +2,10 @@
 const express = require('express');
 const path = require('path');
 
+const publicDirectoryPath = path.join(__dirname, 'public');
 const app = module.exports = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(publicDirectoryPath));
+app.use((req, res) => {
+    res.status(404).contentType('text/plain').send('Not found');
+});
 app.server = app.listen(7030, () => console.log('ViperJS server listening on 7030...'));
