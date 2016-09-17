9e09d3bdb2d0a5562751cd42f2df861279662dde
Boyang <wangboyang1991@gmail.com>
2016-09-17 23:55:23 +0800

    more rules to ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,33 @@
 module.exports = {
-    "extends": "airbnb",
-    "plugins": [
-        "react",
-        "jsx-a11y",
-        "import"
+    'extends': 'airbnb',
+    'plugins': [
+        'react',
+        'jsx-a11y',
+        'import'
     ],
-    "env": {
-        "browser": true,
-        "node": true,
-        "mocha": true
+    'env': {
+        'browser': true,
+        'node': true,
+        'mocha': true
     },
-    "rules": {
-        "react/require-extension": "off",
-        "strict": ["warn", "global"],
-        "no-console": "off",
-        "import/no-extraneous-dependencies": ["warn", {"devDependencies": true}],
-        "indent": ["error", 4],
-        "react/prefer-stateless-function": "off",
-        "class-methods-use-this": "off",
-        "max-len": ["error", {"code": 120, "comments": 120}],
-        "arrow-body-style": "off",
-        "no-fallthrough": "off",
-        "linebreak-style": "off"
+    'rules': {
+        'react/require-extension': 'off',
+        'strict': ['warn', 'global'],
+        'no-console': 'off',
+        'import/no-extraneous-dependencies': ['warn', {'devDependencies': true}],
+        'indent': ['error', 4],
+        'react/prefer-stateless-function': 'off',
+        'class-methods-use-this': 'off',
+        'max-len': ['error', {'code': 120, 'comments': 120}],
+        'arrow-body-style': 'off',
+        'no-fallthrough': 'off',
+        'linebreak-style': 'off',
+        'no-param-reassign': ['error', {'props': false}],
+        'consistent-return': 'off',
+        'no-plusplus': 'off',
+        "no-use-before-define": ["error", { "functions": false, "classes": false }]
     },
-    "parserOptions": {
-        "sourceType": "script"
+    'parserOptions': {
+        'sourceType': 'script'
     }
 };
