f960bce2e7f90c8ad991cc53e81bbe492ac29ef2
Boyang <wangboyang1991@gmail.com>
2016-09-18 00:38:05 +0800

    rewrite reducer, closed #13

--- public/js/main.js
+++ public/js/main.js
@@ -9,6 +9,8 @@ const ReactRedux = require('react-redux');
 const ViperjsApp = require('./components/ViperjsApp.js');
 const ViperjsReducer = require('./reducers/ViperjsReducer.js');
 
+const ViperjsStore = Redux.createStore(ViperjsReducer.reducer, ViperjsReducer.initialState,
+    window.devToolsExtension && window.devToolsExtension());
 ReactDOM.render(React.createElement(ReactRedux.Provider, {
-    store: Redux.createStore(ViperjsReducer.reducer, ViperjsReducer.initialState),
+    store: ViperjsStore,
 }, React.createElement(ViperjsApp)), document.getElementById('viperjs-wrapper'));
--- public/js/reducers/ViperjsReducer.js
+++ public/js/reducers/ViperjsReducer.js
@@ -5,25 +5,60 @@ const viperjsUtil = require('../viperjs-util.js');
 module.exports.initialState = {
     historyInputs: [],
     currentInput: '',
+    historyTokenizeds: [],
+    historyParseds: [],
 };
+const actionReducers = {};
+let nextId = 0;
 module.exports.reducer = (oldState, action) => {
-    const newState = Object.assign({}, oldState);
-    if (action.type === 'input') {
-        const input = action.value;
-        if (input && oldState.currentInput !== input) {
-            newState.currentInput = input;
-            return newState;
-        }
-    } else if (action.type === 'tokenize') {
-        if (oldState.historyInputs[oldState.historyInputs.length - 1] !== oldState.currentInput) {
-            newState.historyInputs = [...oldState.historyInputs, oldState.currentInput];
-            try {
-                newState.outputTokenized = esprima.tokenize(oldState.currentInput);
-            } catch (e) {
-                newState.outputTokenized = e;
-            }
-            return newState;
-        }
+    if (Object.keys(actionReducers).includes(action.type)) {
+        return actionReducers[action.type](oldState, action, Object.assign({}, oldState))
+            || oldState;
     }
     return oldState;
+};
+actionReducers.input = (oldState, action, newState) => {
+    const input = action.value;
+    if (input && oldState.currentInput !== input) {
+        newState.currentInput = input;
+        return newState;
+    }
+};
+actionReducers.tokenize = (oldState, action, newState) => {
+    const lastHistoryInput = viperjsUtil.peek(oldState.historyInputs);
+    if (!lastHistoryInput ||
+        lastHistoryInput.value !== oldState.currentInput) {
+        const currentId = nextId++;
+        newState.historyInputs = [...oldState.historyInputs,
+            { id: currentId, value: oldState.currentInput }];
+        const currentInputObj = { id: currentId };
+        newState.historyTokenizeds = [...oldState.historyTokenizeds,
+            currentInputObj];
+        try {
+            currentInputObj.value =
+                esprima.tokenize(oldState.currentInput);
+        } catch (e) {
+            currentInputObj.value = e;
+        }
+        return newState;
+    }
+};
+actionReducers.parse = (oldState, action, newState) => {
+    const lastTokenized = viperjsUtil.peek(oldState.historyTokenizeds);
+    if (!lastTokenized) {
+        return;
+    }
+    if (!viperjsUtil.peek(oldState.historyParseds) ||
+        viperjsUtil.peek(oldState.historyParseds).id !== lastTokenized.id) {
+        const currentParsedObj = { id: lastTokenized.id };
+        newState.historyParseds = [...oldState.historyParseds, currentParsedObj];
+        try {
+            currentParsedObj.value =
+                esprima.parse(oldState.historyInputs.find(
+                    elem => elem.id === lastTokenized.id).value);
+        } catch (e) {
+            currentParsedObj.value = e;
+        }
+        return newState;
+    }
 };
