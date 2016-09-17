4023b7bd929085af5f5bd9fc7499bcd503db9c44
Boyang <wangboyang1991@gmail.com>
2016-09-17 11:55:45 +0800

    add 200 check for viperjs

--- test/casper/tests.js
+++ test/casper/tests.js
@@ -5,11 +5,20 @@ casper.test.begin('Sanity', 1, function suite(test) {
     test.done();
 });
 casper.test.begin('Google', 1, function suite(test) {
-    casper.start("http://www.google.fr/", function() {
-        test.assertTitle("Google", "title");
-
-    });
-    casper.run(function() {
-        test.done();
-    });
+    casper
+        .start('http://www.google.fr/', function() {
+            test.assertTitle('Google', 'title');
+        })
+        .run(function() {
+            test.done();
+        });
+});
+casper.test.begin('ViperJS', 1, function suite(test) {
+    casper
+        .start('http://localhost:7030/', function() {
+            test.assertHttpStatus(200);
+        })
+        .run(function() {
+            test.done();
+        });
 });
