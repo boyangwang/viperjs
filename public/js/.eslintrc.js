3b7276d0edd23cabb601ab994ecff6489536f306
Boyang <wangboyang1991@gmail.com>
2016-09-19 11:43:42 +0800

    eslintrc is hierachical

--- public/js/.eslintrc.js
+++ public/js/.eslintrc.js
@@ -1,34 +1,5 @@
 module.exports = {
-    'extends': 'airbnb',
-    'plugins': [
-        'react',
-        'jsx-a11y',
-        'import'
-    ],
-    'env': {
-        'browser': true,
-        'node': true,
-        'mocha': true
-    },
-    'rules': {
-        'react/require-extension': 'off',
-        'strict': ['warn', 'global'],
-        'no-console': 'off',
-        'import/no-extraneous-dependencies': ['warn', {'devDependencies': true}],
-        'indent': ['error', 4],
-        'react/prefer-stateless-function': 'off',
-        'class-methods-use-this': 'off',
-        'max-len': ['error', {'code': 120, 'comments': 120}],
-        'arrow-body-style': 'off',
-        'no-fallthrough': 'off',
-        'linebreak-style': 'off',
-        'no-param-reassign': ['error', {'props': false}],
-        'consistent-return': 'off',
-        'no-plusplus': 'off',
-        "no-use-before-define": ["error", { "functions": false, "classes": false }]
-    },
     'parserOptions': {
-        "ecmaVersion": 6,
         'sourceType': 'module'
     }
 };
