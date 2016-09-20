926c083a46f6e9a9687dc6986e2a5a8ac16b9956
Boyang <wangboyang1991@gmail.com>
2016-09-20 10:45:36 +0800

    added flightplan.js

---
+++ flightplan.js
@@ -0,0 +1,23 @@
+const plan = require('flightplan');
+
+plan.target('staging', {
+    host: 'playground.wangboyang.com',
+    port: 22,
+    username: 'root',
+    privateKey: 'C:\\Users\\wangb\\.ssh\\id_rsa',
+});
+const gitDir = '/var/www/viperjs';
+plan.remote(['clean-deploy', 'deploy'], (remote) => {
+    remote.sudo(`npm --prefix ${gitDir} stop`);
+});
+plan.remote(['clean-deploy'], (remote) => {
+    remote.rm(`-rf -- ${gitDir}`);
+    remote.sudo('cd /var/www && git clone git@github.com:boyangwang/viperjs.git');
+});
+plan.remote(['clean-deploy', 'deploy'], (remote) => {
+    remote.sudo(`cd ${gitDir} && git pull origin master`);
+    remote.sudo(`npm --prefix ${gitDir} run full-build`);
+});
+plan.remote(['clean-deploy', 'deploy', 'start'], (remote) => {
+    remote.sudo(`npm --prefix ${gitDir} start`);
+});
