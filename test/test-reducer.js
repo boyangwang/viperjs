275d93740856c703f29dcc17054c2692149c527b
yingchi <yingchi.pei@gmail.com>
2016-09-23 07:16:11 +0800

    Add mocha reducer test (#26)
    * add one more mocha test case for reducer
    
    * add more mocha test cases for reducer toggle actions
    
    * fix typo and add mocha test cases for reducer toggle actions
    
    * compare full state in reducer test
    
    * use full state for reducer test
    
    * add true -> false test cases for toggle action reducer test

--- test/test-reducer.js
+++ test/test-reducer.js
@@ -13,4 +13,38 @@ describe('State reducer', () => {
         const newState = reducer(state, { type: 'xxx', value: 'xxx' });
         expect(newState).equal(state);
     });
+    it('should handle toggleHistoryInputs: false -> true', () => {
+        const expectedNewState = Object.assign({}, state, {isShowHistoryInputs: true});
+        const newState = reducer(state, { type: 'toggleHistoryInputs'});
+        expect(newState).deep.equal(expectedNewState);
+    });
+    it('should handle toggleHistoryInputs: true -> false', () => {
+        const oldState = Object.assign({}, state, {isShowHistoryInputs: true});
+        const expectedNewState = Object.assign({}, state, {isShowHistoryInputs: false});
+        const newState = reducer(oldState, { type: 'toggleHistoryInputs'});
+        expect(newState).deep.equal(expectedNewState);
+    });
+    it('should handle toggleHistoryTokenizeds: false -> true', () => {
+        const expectedNewState = Object.assign({}, state, {isShowHistoryTokenizeds: true});
+        const newState = reducer(state, { type: 'toggleHistoryTokenizeds'});
+        expect(newState).deep.equal(expectedNewState);
+    });
+    it('should handle toggleHistoryTokenizeds: true -> false', () => {
+        const oldState = Object.assign({}, state, {isShowHistoryTokenizeds: true});
+        const expectedNewState = Object.assign({}, state, {isShowHistoryTokenizeds: false});
+        const newState = reducer(oldState, { type: 'toggleHistoryTokenizeds'});
+        expect(newState).deep.equal(expectedNewState);
+    });
+    it('should handle toggleHistoryParseds: false -> true', () => {
+        const expectedNewState = Object.assign({}, state, {isShowHistoryParseds: true});
+        const newState = reducer(state, { type: 'toggleHistoryParseds'});
+        expect(newState).deep.equal(expectedNewState);
+    });
+    it('should handle toggleHistoryParseds: true -> false', () => {
+        const oldState = Object.assign({}, state, {isShowHistoryParseds: true});
+        const expectedNewState = Object.assign({}, state, {isShowHistoryParseds: false});
+        const newState = reducer(oldState, { type: 'toggleHistoryParseds'});
+        expect(newState).deep.equal(expectedNewState);
+    });
 });
+
